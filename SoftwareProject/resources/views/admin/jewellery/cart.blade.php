b8d09f006eccdba3dc62c0eaeefb927dea7196f1
Agne Postnikova <N00211108@iadt.ie>
2023-04-17 14:29:39 +0100

    added cols to make content more narrow

--- SoftwareProject/resources/views/admin/jewellery/cart.blade.php
+++ SoftwareProject/resources/views/admin/jewellery/cart.blade.php
@@ -1,101 +1,97 @@
 @extends('admin.jewellery.shop')
 
 @section('content')
-    <table id="cart" class="table table-bordered m-5">
-        <thead>
-            <tr>
-                <th>Product</th>
-                <th>Price</th>
-                {{-- <th>Total</th> --}}
-                {{-- <th></th> --}}
-            </tr>
-        </thead>
-        <tbody>
-            @php $total = 0 @endphp
-            @if (session('cart'))
-                @foreach (session('cart') as $id => $details)
-                    <tr rowId="{{ $id }}">
-                        <td data-th="Product">
-                            <div class="row">
-                                <div class="col-sm-3 hidden-xs"><img src="{{ asset('storage/images/' . $details['img']) }}"
-                                        class=" w-50" />
+    <div class="col-10 mx-auto">
+        <p class="fs-2 mt-2">Your current shopping cart</p>
+        <table id="cart" class="table table-bordered mt-3">
+            <thead>
+                <tr>
+                    <th>Product</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php $total = 0 @endphp
+                @if (session('cart'))
+                    @foreach (session('cart') as $id => $details)
+                        <tr rowId="{{ $id }}">
+                            <td data-th="Product">
+                                <div class="row">
+                                    <div class="col-sm-3 hidden-xs"><img
+                                            src="{{ asset('storage/images/' . $details['img']) }}" class=" w-75" />
+                                    </div>
+                                    <div class="col-sm-9">
+                                        <h4 class="nomargin">{{ $details['name'] }}</h4>
+                                    </div>
                                 </div>
-                                <div class="col-sm-9">
-                                    <h4 class="nomargin">{{ $details['name'] }}</h4>
-                                </div>
+                            </td>
+                            <td data-th="Price">${{ $details['price'] }}</td>
+                        </tr>
+                    @endforeach
+                @endif
+            </tbody>
+    </div>
+    <tfoot>
+        <tr>
+            <td colspan="5" class="text-right">
+                <a href="{{ url('/admin/jewellery') }}" class="btn btn-secondary"><i class="fa fa-angle-left"></i>
+                    Continue
+                    Shopping</a>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                    data-bs-target="#staticBackdrop">Checkout</button>
+                <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
+                    tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-xl">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <p class="modal-title fs-5" id="staticBackdropLabel"><strong>Card informarion</strong> >
+                                    Billing address > Purchase</p>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
                             </div>
-                        </td>
-                        <td data-th="Price">${{ $details['price'] }}</td>
-
-                        {{-- <td data-th="Subtotal" class="text-center"></td> --}}
-                        {{-- <td class="actions">
-                            <a class="btn btn-outline-danger btn-sm delete-product"><i class="fa fa-trash-o"></i>Delete</a>
-                        </td> --}}
-                    </tr>
-                @endforeach
-            @endif
-        </tbody>
-        <tfoot>
-            <tr>
-                <td colspan="5" class="text-right">
-                    <a href="{{ url('/admin/jewellery') }}" class="btn btn-secondary"><i class="fa fa-angle-left"></i>
-                        Continue
-                        Shopping</a>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                        data-bs-target="#staticBackdrop">Checkout</button>
-                    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
-                        tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-xl">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <p class="modal-title fs-5" id="staticBackdropLabel"><strong>Card informarion</strong> > Billing address > Purchase</p>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                        aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <div class="container-lg py-1">
-                                        <div class="row d-flex justify-content-between">
-                                            <div class="col">
-                                                <h1 class="h4 py-3">Card & Email</h1>
-                                                <div class="col-lg-12 input">
-                                                    <div class="d-flex justify-content-between">
-                                                        <div class="col-lg-5">
-                                                            <p>Name</p>
-                                                            <div class="input-group mb-3">
-                                                                <input type="text" class="form-control" />
-                                                            </div>
-                                                        </div>
-                                                        <div class="col-lg-5">
-                                                            <p>surname</p>
-                                                            <div class="input-group mb-3">
-                                                                <input type="text" class="form-control" />
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-lg-12">
-                                                        <p>e-mail</p>
+                            <div class="modal-body">
+                                <div class="container-lg py-1">
+                                    <div class="row d-flex justify-content-between">
+                                        <div class="col">
+                                            <h1 class="h4 py-3">Card & Email</h1>
+                                            <div class="col-lg-12 input">
+                                                <div class="d-flex justify-content-between">
+                                                    <div class="col-lg-5">
+                                                        <p>Name</p>
                                                         <div class="input-group mb-3">
                                                             <input type="text" class="form-control" />
                                                         </div>
                                                     </div>
-                                                    <div class="col-lg-12">
-                                                        <p>card number</p>
+                                                    <div class="col-lg-5">
+                                                        <p>surname</p>
                                                         <div class="input-group mb-3">
                                                             <input type="text" class="form-control" />
                                                         </div>
                                                     </div>
-                                                    <div class="d-flex justify-content-between">
-                                                        <div class="col-lg-5">
-                                                            <p>EXPIRATION</p>
-                                                            <div class="input-group mb-3">
-                                                                <input type="text" class="form-control" />
-                                                            </div>
+                                                </div>
+                                                <div class="col-lg-12">
+                                                    <p>e-mail</p>
+                                                    <div class="input-group mb-3">
+                                                        <input type="text" class="form-control" />
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-12">
+                                                    <p>card number</p>
+                                                    <div class="input-group mb-3">
+                                                        <input type="text" class="form-control" />
+                                                    </div>
+                                                </div>
+                                                <div class="d-flex justify-content-between">
+                                                    <div class="col-lg-5">
+                                                        <p>EXPIRATION</p>
+                                                        <div class="input-group mb-3">
+                                                            <input type="text" class="form-control" />
                                                         </div>
-                                                        <div class="col-5">
-                                                            <p>cvc</p>
-                                                            <div class="input-group mb-3">
-                                                                <input type="text" class="form-control" />
-                                                            </div>
+                                                    </div>
+                                                    <div class="col-5">
+                                                        <p>cvc</p>
+                                                        <div class="input-group mb-3">
+                                                            <input type="text" class="form-control" />
                                                         </div>
                                                     </div>
                                                 </div>
@@ -103,16 +99,17 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Continue Purchase</button>
-                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Continue Purchase</button>
                             </div>
                         </div>
                     </div>
-                </td>
-            </tr>
-        </tfoot>
+                </div>
+            </td>
+        </tr>
+    </tfoot>
     </table>
 @endsection
 
--- SoftwareProject/resources/views/admin/jewellery/create.blade.php
+++ SoftwareProject/resources/views/admin/jewellery/create.blade.php
@@ -1,69 +1,69 @@
 @extends('layouts.app')
 
 @section('content')
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Jewellery') }}
-        </h2>
-    </x-slot>
-
     <form action="{{ route('admin.jewellery.store') }}" method="post" enctype="multipart/form-data"
         class="bg-transparent my-6 p-6">
         @csrf
         <div class="container fs-4">
-            <div class="row">
-                <div class="col-12">
-                    <h2 class=" mt-3 mb-5 text-4xl">Upload your jewellery</h2>
+            <div class="col-10 mx-auto">
+                <div class="row">
+                    <div class="col-12">
+                        <h2 class=" mt-3 mb-5 text-4xl">Upload your jewellery</h2>
+                    </div>
+
+                    <div class="col-3">
+                        <p>Name of jewellery:</p>
+                        <x-input type="text" name="name" field="name" placeholder="name"
+                            class="w-full border-0 rounded-0 border-bottom" autocomplete="off" :value="@old('name')">
+                        </x-input>
+
+                        <p class="mt-6">Price:</p>
+                        <x-input type="decimal" name="price" field="price" placeholder="€00.00"
+                            class="w-auto  border-0 rounded-0 border-bottom" autocomplete="off" :value="@old('price')">
+                        </x-input>
+                    </div>
+                    <div class="col-9">
+                        <p class="mt-6">Description:</p>
+                        <x-textarea name="description" rows="10" field="description" placeholder="describe here"
+                            class="w-full mt-6" :value="@old('description')">
+                        </x-textarea>
+                    </div>
+
+                    <div class="col-3">
+                        <p class="mt-6">Upload your image:</p>
+                        <x-file-input type="file" name="img" placeholder="image" class="w-full mt-6" field="img">
+                        </x-file-input>
+                    </div>
+                    <div class="col fs-5">
+                        <p class="mt-6 fs-4">Select Category:</p>
+                        <select class="form-select" field="category" name="category" label="Choose category">
+                            @foreach ($categories as $category)
+                                <option value="{{ $category }}">
+                                    {{ $category }}
+                                </option>
+                            @endforeach
+                        </select>
+                        @error('category')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="col fs-5">
+                        <p class="mt-6 fs-4">Select Material:</p>
+                        <select class="form-select" field="material" name="material" label="Choose material">
+                            @foreach ($materials as $material)
+                                <option value="{{ $material }}">
+                                    {{ $material }}
+                                </option>
+                            @endforeach
+                        </select> @error('material')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="d-flex justify-content-end">
+                        <x-primary-button>Save and Upload</x-primary-button>
+                    </div>
                 </div>
 
-                <div class="col-3">
-                    <p>Name of jewellery:</p>
-                    <x-input type="text" name="name" field="name" placeholder="name"
-                        class="w-full border-0 rounded-0 border-bottom" autocomplete="off" :value="@old('name')">
-                    </x-input>
-
-                    <p class="mt-6">Price:</p>
-                    <x-input type="decimal" name="price" field="price" placeholder="€00.00"
-                        class="w-auto  border-0 rounded-0 border-bottom" autocomplete="off" :value="@old('price')">
-                    </x-input>
-                </div>
-                <div class="col-9">
-                    <p class="mt-6">Description:</p>
-                    <x-textarea name="description" rows="10" field="description" placeholder="describe here"
-                        class="w-full mt-6" :value="@old('description')">
-                    </x-textarea>
-                </div>
-
-                <div class="col-3">
-                    <p class="mt-6">Upload your image:</p>
-                    <x-file-input type="file" name="img" placeholder="image" class="w-full mt-6" field="img">
-                    </x-file-input>
-                </div>
-                <div class="col fs-5">
-                    <p class="mt-6 fs-4">Select Category:</p>
-                    @foreach ($categories as $category)
-                        <input type="radio", value="{{ $category }}"
-                            {{ old('category') == $category ? 'selected' : '' }} name="category">
-                        {{ $category }} <br>
-                    @endforeach
-                    @error('category')
-                        <div class="text-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="col fs-5">
-                    <p class="mt-6 fs-4">Select Material:</p>
-                    @foreach ($materials as $material)
-                        <input type="radio", value="{{ $material }}"
-                            {{ old('material') == $material ? 'selected' : '' }} name="material">
-                        {{ $material }} <br>
-                    @endforeach
-                    @error('material')
-                        <div class="text-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="col-12">
-                    <x-primary-button>Save and Upload</x-primary-button>
-                </div>
             </div>
         </div>
     </form>
