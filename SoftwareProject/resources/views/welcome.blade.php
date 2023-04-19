dc2190aa2b2ce6ef125b7a947597502c469ffe47
Agne Postnikova <N00211108@iadt.ie>
2023-04-19 14:57:03 +0100

    Changed the view of welcome page

--- SoftwareProject/resources/views/welcome.blade.php
+++ SoftwareProject/resources/views/welcome.blade.php
@@ -1,86 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container mx-5">
-        <div class="row justify-content-center">
-            {{-- @foreach ($jewellery as $piece)
-            <div class="card rounded-0 border-0 bg-transparent" style="width: 17rem";>
-                <a href="{{ route('user.jewellery.show', $piece) }}"><img
-                        src="{{ asset('storage/images/' . $piece->img) }}"
-                        class="card-img-top rounded-0 border border-dark" alt="..."></a>
-                <div class="card-body px-0 pt-1">
-                    <h3 class="fs-4 text-black">
-                        <a class="link-dark"
-                            href="{{ route('user.jewellery.show', $piece) }}">{{ $piece->name }}</a>
-                    </h3>
-                    <p class="card-text text-black fs-5 mt-2">€{{ $piece->price }}</p>
-                </div>
-            </div>
-        @endforeach --}}
-
-            <div>
-                <h1>Hello World</h1>
-                <h3>This is a laravel-bootstrap template</h3>
-                <div class="mt-5">
-                    <button type="button" class="btn btn-primary">Primary</button>
-                    <button type="button" class="btn btn-secondary">Secondary</button>
-                    <button type="button" class="btn btn-success">Success</button>
-                    <button type="button" class="btn btn-danger">Danger</button>
-                    <button type="button" class="btn btn-warning">Warning</button>
-                    <button type="button" class="btn btn-info">Info</button>
-                    <button type="button" class="btn btn-light">Light</button>
-                    <button type="button" class="btn btn-dark">Dark</button>
-
-                    <button type="button" class="btn btn-link">Link</button>
-                </div>
-
-                <div class="row row-cols-1 row-cols-md-3 g-4 mt-5">
-                    <div class="col">
-                        <div class="card">
-                            <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-social.png"
-                                class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card">
-                            <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-social.png"
-                                class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card">
-                            <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-social.png"
-                                class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                                    to additional content.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card">
-                            <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-social.png"
-                                class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="col text-center mt-5 ">
+        <img src="{{ asset('storage/images/LOGO.png') }}" class="w-25 rounded-5" alt="..." />
+        <p class="fs-3 py-4 ">Please register or log in <br> to be able to upload browse our trinkets!</p>
     </div>
 @endsection
